52843e4d84ca9af79c853ae98c834fa10e9c39ae
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-05 14:03:58 -0300

    Versão G 1.1

--- src/view/form-cliente-login.php
+++ src/view/form-cliente-login.php
@@ -1,37 +1,107 @@
 <?php include "view/header.php";?>
-<form method="post" action="cliente">
-<fieldset>
 
-<!-- Form Name -->
-<h2>LOGIN
-SOU CLIENTE</h2>
 
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label badge badge-light" for="name">Nome</label>  
-  <div class="col-md-4">
-  <input id="nome" name="nome" type="text" placeholder="Nome" class="form-control input-md" required="">
+
+<div class="container">
+<link rel="stylesheet" href="../2css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <div class="content first-content">
+            <div class="first-column">
+                <h2 class="title title-primary">Bem-vindo de volta!</h2>
+                <p class="description description-primary">Para manter-se conectado conosco</p>
+                <p class="description description-primary">faça o login com suas informações pessoais</p>
+                <button id="signin" class="btn btn-primary">Logar</button>
+            </div>    
+            <div class="second-column">
+                <h2 class="title title-second">Criar Conta</h2>
+                <div class="social-media">
+                    <ul class="list-social-media">
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-facebook-f"></i>        
+                            </li>
+                        </a>
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-google-plus-g"></i>
+                            </li>
+                        </a>
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-linkedin-in"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div><!-- social media -->
+                <p class="description description-second">Ou use seu email para registrar:</p>
+                <form class="form">
+                    <label class="label-input" for="">
+                        <i class="far fa-user icon-modify"></i>
+                        <input type="text" placeholder="Nome">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" placeholder="Email">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Senha">
+                    </label>
+                    
+                    
+                    <button class="btn btn-second">Registrar</button>        
+                </form>
+            </div><!-- second column -->
+        </div><!-- first content -->
+        <div class="content second-content">
+            <div class="first-column">
+                <h2 class="title title-primary">Olá :)</h2>
+                <p class="description description-primary">Insira seus dados pessoais</p>
+                <p class="description description-primary">Começe a assitir seus filmes conosco</p>
+                <button id="signup" class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="second-column">
+                <h2 class="title title-second">Logue com sua conta </h2>
+                <div class="social-media">
+                    <ul class="list-social-media">
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-facebook-f"></i>
+                            </li>
+                        </a>
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-google-plus-g"></i>
+                            </li>
+                        </a>
+                        <a class="link-social-media" href="#">
+                            <li class="item-social-media">
+                                <i class="fab fa-linkedin-in"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div><!-- social media -->
+                <p class="description description-second">ou use sua conta do site:</p>
+                <form class="form">
+                
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" placeholder="Email">
+                    </label>
+                
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Password">
+                    </label>
+                
+                    <a class="password" href="#">Esqueceu sua senha ?</a>
+                    <button class="btn btn-second">Logar</button>
+                </form>
+            </div><!-- second column -->
+        </div><!-- second-content -->
     
-  </div>
-</div>
-
-<!-- Password input-->
-<div class="form-group">
-  <label class="col-md-4 control-label badge badge-dark" for="senha">Senha</label>
-  <div class="col-md-4">
-    <input id="senha" name="senha" type="password" placeholder="Senha" class="form-control input-md" required="">
-    
-  </div>
-</div>
-
-<!-- Button -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="singlebutton"></label>
-  <div class="col-md-4">
-    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Entrar</button>
-  </div>
-</div>
-
-</fieldset>
-</form>
+    <script src="../3js/app.js"></script>
 <?php include "view/footer.php";?>
