d8202d8a89c5203f8a8050dd41ddec5f289f8f79
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-06 16:28:40 -0300

    versao C2

--- src/Model/conexao.php
+++ src/Model/conexao.php
@@ -8,7 +8,7 @@
         }
         public static function getInstance(){
             try {
-                 $conexao = new \PDO("mysql:host={$serverHost}; dbname={$serverDB}", "{$serverUser}", "{$serverPass}");
+                 $conexao = new \PDO("mysql:host=localhost; dbname=bigode", "root", "");
                // $conexao = new \PDO("mysql:host=mysql18-farm70.uni5.net; dbname=qualificarofic", "qualificarofic", "senac10058");
                 $conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 $conexao->exec("set names utf8");
