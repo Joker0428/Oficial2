d4f662d7745cbbd6ad384a38366d9132044ef628
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-12-12 16:07:00 -0300

    Versão G 2.4.0

--- www/View/header-adm.php
+++ www/View/header-adm.php
@@ -82,17 +82,7 @@
           
         </div>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Settings
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item " href="<?php echo $url;?>/fornecedor/listar">Lista dos Fornecedores</a>
-          <a class="dropdown-item " href="<?php echo $url;?>/fornecedor/cadastrar">Registrar Fornecedor</a>
-          
-          
-        </div>
-      </li>
+    
      
     </ul>
    
--- www/View/painel-adm.php
+++ www/View/painel-adm.php
@@ -4,14 +4,14 @@
 
 <!-- Example single danger button -->
 <div class="btn-group p">
-  <button type="button" class="btn btn-primary dropdown-toggle p" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  Notifications <span class="badge badge-light">4</span>
+  <button type="button" class="btn btn-primary dropdown-toggle p" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-cog"></i>
+  Settings <span class="badge badge-light">4</span>
   </button>
   <div class="dropdown-menu">
     <p>O Cliente Coringa Acaba de efetua um Pedido Pelo serviço de alinhamento</p>
     <hr>
-    <a class="dropdown-item  azul" href="#">Verificar</a>
-  
+    <a class="dropdown-item  azul1" href="#">Verificar</a>
+    <a class="dropdown-item azul" href="http://localhost/Oficial2/www/paineladm/logoff"><i class="fas fa-sign-out-alt"></i>Sair</a>
   </div>
 </div>
 
@@ -19,7 +19,7 @@
     <!-- Button trigger modal -->
 
    <p></p>
-    <a class="btn btn-primary" href="http://localhost/Oficial2/www/paineladm/logoff">Sair</a>
+    
 
 
   
